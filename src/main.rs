0ce1a9537b0e8d06188e8f84362423382cffd0d1
Lucas Walter <wsacul@gmail.com>
2025-09-15 12:12:35 -0700

    now have physics example inside a class

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,18 @@ mod ffi {
     unsafe extern "C++" {
         /*
         include!("jolt_rust_cpp/src/vehicle.h");
-        type SimSystem;
-        fn new_sim_system() -> UniquePtr<SimSystem>;
         */
         include!("jolt_rust_cpp/src/misc.h");
-        fn init() -> i64;
+        type SimSystem;
+        fn new_sim_system(max_num_bodies: u32) -> UniquePtr<SimSystem>;
+        // fn init(max_num_bodies: u32) -> i64;
     }
 }
 
 fn main() {
     println!("jolt_rust_cpp");
-    // let _sim_system = ffi::new_sim_system();
-    ffi::init();
+    let sim_system = ffi::new_sim_system(8000);
+    // sim_system.init(8000);
+    // ffi::init(8000);
     println!("done");
 }
--- src/misc.h
+++ src/misc.h
@@ -1,5 +1,12 @@
 #include <cstdint>
+#include <memory>
 
 namespace jolt_rust_cpp {
-  int64_t init();
-}
+  class SimSystem {
+    public:
+      SimSystem(uint32_t max_num_bodies);
+      int64_t init(uint32_t max_num_bodies);
+  };
+
+  std::unique_ptr<SimSystem> new_sim_system(uint32_t max_num_bodies);
+} // namespace jolt_rust_cpp
